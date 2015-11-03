98ccf57468f5da921619c6195998eb9c3f144ec3
Joke-Dk <dking@live.com>
2015-11-03 14:08:27 +0800

    Add the two comparison scene and gitignore file

---
+++ 01-SearchLight/rendering.bat
@@ -0,0 +1,8 @@
+"../dist/mitsuba.exe" "./vm/searchlight.xml"
+
+"../dist/mitsuba.exe" "./uvm/searchlight.xml"
+
+"../dist/mitsuba.exe" "./vcm/searchlight.xml"
+
+"../dist/mitsuba.exe" "./uvcm/searchlight.xml"
+
---
+++ 02-DiscoVol-PointLight/rendering.bat
@@ -0,0 +1,8 @@
+"../../dist/mitsuba.exe" "./uvm/discoVol_vcmm.xml"
+
+"../../dist/mitsuba.exe" "./vc/discoVol_vcmm.xml"
+
+"../../dist/mitsuba.exe" "./vm/discoVol_vcmm.xml"
+
+"../../dist/mitsuba.exe" "./vc_long/discoVol_vcmm.xml"
+
